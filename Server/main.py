e643af22d4f181f21586e754d176a435b0f713d3
NanocomStudios <dulararupasinghe@gmail.com>
2026-07-26 21:30:58 +0530

    Backend almost done without redundency

--- Server/main.py
+++ Server/main.py
@@ -17,14 +17,6 @@ from fastapi.middleware.cors import CORSMiddleware
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-# message = messaging.Message(
-#     notification=messaging.Notification(title="Hello", body="Push via Python!"),
-#     token="YOUR_DEVICE_REGISTRATION_TOKEN", # Target specific device
-# )
-
-# response = messaging.send(message)
-# print("Successfully sent message:", response)
-
 
 # message = messaging.Message(
 #     notification=messaging.Notification(title="Broadcast", body="Hello users!"),
@@ -35,16 +27,44 @@ firebase_admin.initialize_app(cred)
 conn = sqlite3.connect('items.db')
 c = conn.cursor()
 
-c.execute("CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)")
-c.execute("CREATE TABLE IF NOT EXISTS sessions (session_id TEXT PRIMARY KEY, userID INTEGER, FOREIGN KEY(userID) REFERENCES users(userID))")
+c.execute("CREATE TABLE IF NOT EXISTS users (\
+            userID INTEGER PRIMARY KEY AUTOINCREMENT,\
+            username TEXT UNIQUE, password TEXT\
+          )")
+
+c.execute("CREATE TABLE IF NOT EXISTS sessions (\
+            sessionID TEXT PRIMARY KEY,\
+            userID INTEGER NOT NULL,\
+            expire TEXT DEFAULT (datetime('now', '+2 days')),\
+            deviceID TEXT NOT NULL,\
+            FOREIGN KEY(userID) REFERENCES users(userID)\
+          )")
 
 c.execute("CREATE TABLE IF NOT EXISTS items (\
-    itemID INTEGER PRIMARY KEY,\
-    itemName TEXT NOT NULL,\
-    type TEXT NOT NULL,\
-    state TEXT NOT NULL DEFAULT '0',\
-    lastOnTime TEXT NOT NULL DEFAULT '0',\
-    cuttoffTime TEXT NOT NULL DEFAULT '0')")
+            itemID INTEGER PRIMARY KEY,\
+            itemName TEXT NOT NULL,\
+            type TEXT NOT NULL,\
+            state TEXT NOT NULL DEFAULT '0',\
+            lastOnTime TEXT NOT NULL DEFAULT '0',\
+            cuttoffTime TEXT NOT NULL DEFAULT '0'\
+          )")
+
+c.execute("CREATE TABLE IF NOT EXISTS rooms (\
+            roomID INTEGER PRIMARY KEY AUTOINCREMENT,\
+            roomName TEXT NOT NULL,\
+            userID INTEGER NOT NULL,\
+            FOREIGN KEY(userID) REFERENCES users(userID)\
+          )")
+
+c.execute("CREATE TABLE IF NOT EXISTS room_items (\
+            roomID INTEGER NOT NULL,\
+            itemID INTEGER NOT NULL,\
+            PRIMARY KEY (roomID, itemID),\
+            FOREIGN KEY(roomID) REFERENCES rooms(roomID),\
+            FOREIGN KEY(itemID) REFERENCES items(itemID)\
+            )")
+
+
 
 # Configuration settings
 # Replace with your HiveMQ Cloud Cluster URL (e.g., "xxxxxx.s1.eu.hivemq.cloud")
@@ -104,9 +124,9 @@ app.add_middleware(
 class User(BaseModel):
     username: str
     password: str
+    deviceID: str
 
 class Session(BaseModel):
-    userID: int
     sessionID: str
 
 class Item(BaseModel):
@@ -114,7 +134,22 @@ class Item(BaseModel):
     itemName: str
     itemType: str
 
-class Action(BaseModel):
+class AppAction(BaseModel):
+    itemID: int
+    action: str
+    value: int
+    sessionID: str
+
+class AppRoom(BaseModel):
+    roomName: str
+    sessionID: str
+
+class RoomItem(BaseModel):
+    roomName: str
+    itemID: int
+    sessionID: str
+
+class ItemAction(BaseModel):
     itemID: int
     action: str
     value: int
@@ -123,6 +158,30 @@ class Update(BaseModel):
     itemID: int
     value: int
 
+def isSessionValid(sessionID: str):
+    conn = sqlite3.connect('items.db')
+    c = conn.cursor()
+    try:
+        c.execute("DELETE FROM sessions WHERE expire < datetime('now')")
+        conn.commit()
+        c.execute("SELECT * FROM sessions WHERE sessionID=?", (sessionID,))
+        result = c.fetchone()
+
+        if result:
+            c.execute("UPDATE sessions SET expire=(datetime('now', '+2 days')) WHERE sessionID=?", (sessionID,))
+            conn.commit()
+
+        conn.close()
+        return result is not None
+    except sqlite3.IntegrityError:
+        print("Error: session validation failed!")
+        conn.close()
+        return False
+
+
+    
+    
+
 @app.post("/user/login")
 def login(user: User):
     conn = sqlite3.connect('items.db')
@@ -130,11 +189,11 @@ def login(user: User):
     c.execute("SELECT * FROM users WHERE username=? AND password=?", (user.username, user.password))
     result = c.fetchone()
     if result:
-        session_id = hashlib.sha256((user.username + user.password + str(time.time())).encode()).hexdigest()
-        c.execute("INSERT OR REPLACE INTO sessions (session_id, userID) VALUES (?, ?)", (session_id, result[0]))
+        sessionID = hashlib.sha256((user.username + user.password + user.deviceID + str(time.time())).encode()).hexdigest()
+        c.execute("INSERT OR REPLACE INTO sessions (sessionID, userID, deviceID) VALUES (?, ?, ?)", (sessionID, result[0], user.deviceID))
         conn.commit()
         conn.close()
-        return {"response": "success", "sessionID" : session_id, "userID": result[0]}
+        return {"response": "success", "sessionID" : sessionID, "userID": result[0]}
     else:
         conn.close()
         return {"response": "failure"}
@@ -176,12 +235,34 @@ def itemRegister(item: Item):
             return {"response": "failure", "error": "Error registering the item!"}
 
 @app.post("/item/action")
-def itemAction(action: Action):
+def itemAction(action: ItemAction):
     conn = sqlite3.connect('items.db')
     c = conn.cursor()
     try:
         c.execute("UPDATE items SET state=? WHERE itemID=?", (str(action.value), action.itemID))
         conn.commit()
+
+        c.execute("SELECT * FROM room_items WHERE itemID=?", (action.itemID,))
+        result = c.fetchall()
+        for row in result:
+            roomID = row[0]
+            c.execute("SELECT * FROM rooms WHERE roomID=?", (roomID,))
+            room = c.fetchone()
+            userID = room[2]
+            c.execute("SELECT * FROM sessions WHERE userID=?", (userID,))
+            sessions = c.fetchall()
+            for session in sessions:
+                sessionID = session[0]
+                message = messaging.Message(
+                    notification=messaging.Notification(title="Action", body= "item/" + str(action.itemID) + " : " + action.action + " : " + str(action.value)),
+                    token=session[3], # Target specific device
+                )
+                try:
+                    response = messaging.send(message)
+                    print("Successfully sent message:", response)
+                except Exception as e:
+                    print("Error sending message:", e)
+
         conn.close()
 
         payload = {"action":action.action, "value": action.value}
@@ -196,9 +277,229 @@ def itemAction(action: Action):
 
 
 @app.post("/item/update")
-def itemAction(update: Update):
-    payload = {"action":action.action, "value": action.value}
-    client.publish("item/" + str(action.itemID) , payload=json.dumps(payload), qos=1)
+def itemUpdate(update: Update):
+    pass
+
+
+@app.post("/app/action")
+def appAction(action: AppAction):
+
+    is_valid = isSessionValid(action.sessionID)
+
+    conn = sqlite3.connect('items.db')
+    c = conn.cursor()
+    try:
+
+        if(is_valid):
+
+            c.execute("UPDATE items SET state=? WHERE itemID=?", (str(action.value), action.itemID))
+            conn.commit()
+
+            c.execute("SELECT * FROM room_items WHERE itemID=?", (action.itemID,))
+            result = c.fetchall()
+            for row in result:
+                roomID = row[0]
+                c.execute("SELECT * FROM rooms WHERE roomID=?", (roomID,))
+                room = c.fetchone()
+                userID = room[2]
+                c.execute("SELECT * FROM sessions WHERE userID=?", (userID,))
+                sessions = c.fetchall()
+                for session in sessions:
+                    sessionID = session[0]
+                    message = messaging.Message(
+                        notification=messaging.Notification(title="Action", body= "item/" + str(action.itemID) + " : " + action.action + " : " + str(action.value)),
+                        token=session[3], # Target specific device
+                    )
+                    try:
+                        response = messaging.send(message)
+                        print("Successfully sent message:", response)
+                    except Exception as e:
+                        print("Error sending message:", e)
+
+            payload = {"action":action.action, "value": action.value}
+            client.publish("item/" + str(action.itemID) , payload=json.dumps(payload), qos=1)
+
+            conn.commit()
+            conn.close()
+
+            return {"response": "success", "action":action.action, "value": action.value}
+        else:
+            conn.close()
+            return {"response": "failure", "error": "Invalid session ID!"}
+    except sqlite3.IntegrityError:
+        conn.close()
+        return {"response": "failure", "error": "Error: action on the item!"}
+
+@app.post("/app/newRoom")
+def appNewRoom(room: AppRoom):
+
+    is_valid = isSessionValid(room.sessionID)
+
+    conn = sqlite3.connect('items.db')
+    c = conn.cursor()
+    try:
+
+        if(is_valid):
+            c.execute("SELECT * FROM sessions WHERE sessionID=?", (room.sessionID,))
+            session = c.fetchone()
+            userID = session[1]
+
+            c.execute("INSERT INTO rooms (roomName, userID) VALUES (?, ?)", (room.roomName, userID))
+            conn.commit()
+            conn.close()
+
+            return {"response": "success", "roomName": room.roomName}
+        else:
+            conn.close()
+            return {"response": "failure", "error": "Invalid session ID!"}
+    except sqlite3.IntegrityError:
+        conn.close()
+        return {"response": "failure", "error": "Error: creating new room!"}
+
+@app.post("/app/deleteRoom")
+def appDeleteRoom(room: AppRoom):
+
+    is_valid = isSessionValid(room.sessionID)
+
+    conn = sqlite3.connect('items.db')
+    c = conn.cursor()
+    try:
+
+        if(is_valid):
+            c.execute("SELECT * FROM sessions WHERE sessionID=?", (room.sessionID,))
+            session = c.fetchone()
+            userID = session[1]
+
+            c.execute("SELECT * FROM room_items WHERE roomID=(SELECT roomID FROM rooms WHERE roomName=? AND userID=?)", (room.roomName, userID))
+            items = c.fetchall()
+            for item in items:
+                c.execute("DELETE FROM room_items WHERE roomID=? AND itemID=?", (item[0], item[1]))
+
+            c.execute("DELETE FROM rooms WHERE roomName=? AND userID=?", (room.roomName, userID))
+            conn.commit()
+            conn.close()
+
+            return {"response": "success", "roomName": room.roomName}
+        else:
+            conn.close()
+            return {"response": "failure", "error": "Invalid session ID!"}
+    except sqlite3.IntegrityError:
+        conn.close()
+        return {"response": "failure", "error": "Error: deleting the room!"}
+
+@app.post("/app/addItemToRoom")
+def appAddItemToRoom(roomItem: RoomItem):
+
+    is_valid = isSessionValid(roomItem.sessionID)
+
+    conn = sqlite3.connect('items.db')
+    c = conn.cursor()
+    try:
+        if(is_valid):
+
+            c.execute("SELECT * FROM items WHERE itemID=?", (roomItem.itemID,))
+            item = c.fetchone()
+            if not item:
+                conn.close()
+                return {"response": "failure", "error": "Item not found!"}
+
+            c.execute("SELECT * FROM room_items WHERE itemID=?", (roomItem.itemID,))
+            item = c.fetchone()
+            if item:
+                conn.close()
+                return {"response": "failure", "error": "Item already added to a room!"}
+
+        
+            c.execute("SELECT * FROM sessions WHERE sessionID=?", (roomItem.sessionID,))
+            session = c.fetchone()
+            userID = session[1]
+
+            c.execute("SELECT * FROM rooms WHERE roomName=? AND userID=?", (roomItem.roomName, userID))
+            room = c.fetchone()
+            if room:
+                roomID = room[0]
+                c.execute("INSERT INTO room_items (roomID, itemID) VALUES (?, ?)", (roomID, roomItem.itemID))
+                conn.commit()
+                conn.close()
+
+                return {"response": "success", "roomName": roomItem.roomName, "itemID": roomItem.itemID}
+            else:
+                conn.close()
+                return {"response": "failure", "error": "Room not found!"}
+        else:
+            conn.close()
+            return {"response": "failure", "error": "Invalid session ID!"}
+    except sqlite3.IntegrityError:
+        conn.close()
+        return {"response": "failure", "error": "Error: adding item to the room!"}
+
+@app.post("/app/removeItemFromRoom")
+def appRemoveItemFromRoom(roomItem: RoomItem):
+    is_valid = isSessionValid(roomItem.sessionID)
+
+    conn = sqlite3.connect('items.db')
+    c = conn.cursor()
+    try:
+        if(is_valid):
+
+            c.execute("SELECT * FROM sessions WHERE sessionID=?", (roomItem.sessionID,))
+            session = c.fetchone()
+            userID = session[1]
+
+            c.execute("SELECT * FROM rooms WHERE roomName=? AND userID=?", (roomItem.roomName, userID))
+            room = c.fetchone()
+            if room:
+                roomID = room[0]
+                c.execute("DELETE FROM room_items WHERE roomID=? AND itemID=?", (roomID, roomItem.itemID))
+                conn.commit()
+                conn.close()
+
+                return {"response": "success", "roomName": roomItem.roomName, "itemID": roomItem.itemID}
+            else:
+                conn.close()
+                return {"response": "failure", "error": "Room not found!"}
+        else:
+            conn.close()
+            return {"response": "failure", "error": "Invalid session ID!"}
+    except sqlite3.IntegrityError:
+        conn.close()
+        return {"response": "failure", "error": "Error: removing item from the room!"}
+
+
+@app.post("/app/getRooms")
+def appGetRooms(session: Session):
+
+    is_valid = isSessionValid(session.sessionID)
+
+    conn = sqlite3.connect('items.db')
+    c = conn.cursor()
+    try:
+        if(is_valid):
+
+            c.execute("SELECT * FROM sessions WHERE sessionID=?", (session.sessionID,))
+            session_data = c.fetchone()
+            userID = session_data[1]
+
+            c.execute("SELECT * FROM rooms WHERE userID=?", (userID,))
+            rooms = c.fetchall()
+
+            room_list = []
+            for room in rooms:
+                roomID = room[0]
+                roomName = room[1]
+                c.execute("SELECT itemID FROM room_items WHERE roomID=?", (roomID,))
+                items = c.fetchall()
+                itemIDs = [item[0] for item in items]
+                room_list.append({"roomName": roomName, "itemIDs": itemIDs})
+
+            conn.close()
+            return {"response": "success", "rooms": room_list}
+        else:
+            conn.close()
+            return {"response": "failure", "error": "Invalid session ID!"}
+    except sqlite3.IntegrityError:
+        conn.close()
+        return {"response": "failure", "error": "Error: retrieving rooms!"}
 
 # try:
     # 7. Publish messages periodically
